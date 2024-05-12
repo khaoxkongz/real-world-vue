55f1503a01f3ed15d88028bca5bc3e4905fe6f64
khaoxkongz <thanapat.makkawal@gmail.com>
2024-05-12 19:50:30 +0700

    feature: local axios fetch data

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios'; // <--- brings in the axios library
 
 export default {
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Does a get request
+      .then((response) => {
+        console.log(response.data); // For now, logs out the response
+        this.events = response.data; // <--- set the events when returned
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response); // Logs out the error
+      });
   },
 };
 </script>
